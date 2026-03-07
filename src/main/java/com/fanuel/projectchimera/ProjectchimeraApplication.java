92079e88bdb63fbc48f162d44003ef654f322a9b
fanuelabebe <fanuelabebe@gmail.com>
2026-03-07 20:23:05 +0300

    Initial Spring Boot Chimera project

---
+++ src/main/java/com/fanuel/projectchimera/ProjectchimeraApplication.java
@@ -0,0 +1,13 @@
+package com.fanuel.projectchimera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectchimeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectchimeraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fanuel/projectchimera/ProjectchimeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanuel.projectchimera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectchimeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
